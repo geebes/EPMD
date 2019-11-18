49123e0290a9169fb1be2bdf363e31ec319d157a
Ben Ward <b.a.ward@soton.ac.uk>
2019-11-18 19:48:28 +0000

    Added empty Output dir

--- run_EPMD_spmd.m
+++ run_EPMD_spmd.m
@@ -8,7 +8,7 @@ addpath EPMD_functions
 %% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 % set options
-run_options.TM_scheme       = 'GUD_X17_weighted_transport'; % 'surface_transport' or 'GUD_X01_weighted_transport', or similar
+run_options.TM_scheme       = 'GUD_X01_weighted_transport'; % 'surface_transport' or 'GUD_X01_weighted_transport', or similar
 run_options.solver          = 'parallel';	% serial or parallel
 run_options.seed_dist       = 'neutral';    % 'preadapted', 'equal', 'singleancestor', 'neutral', 'mutation_dispersal'
 run_options.trajectory      = 'stochastic'; % 'stochastic' or 'deterministic'
@@ -16,7 +16,7 @@ run_options.annual_cycle    = 'static';     % 'static' or 'seasonal'
 
 % note depth-integrated TMs are each associated with a particular abundance distribution
 % (this will be overwritten if using depth-integrated biomass-weighted transport)
-run_options.DARWIN_pop      = 'X17'; 
+run_options.DARWIN_pop      = 'X01'; 
 
 run_options.mutation        = false;      	% allow mutations
 run_options.selection       = false;
