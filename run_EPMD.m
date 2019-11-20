a2b43c594d86a61d356699aa5ea6edfb4d684b12
Ben Ward <b.a.ward@soton.ac.uk>
2019-11-20 13:41:11 +0000

    Changes to run script

--- run_EPMD.m
+++ run_EPMD.m
@@ -9,7 +9,7 @@ addpath EPMD_functions
 
 % set options
 run_options.TM_scheme       = 'GUD_X01_weighted_transport'; % 'surface_transport' or 'GUD_X01_weighted_transport', or similar
-run_options.seed_dist       = 'lineages';    % 'preadapted', 'equal', 'lineages', 'neutral'
+run_options.seed_dist       = 'selective_dispersal';    % 'preadapted', 'equal', 'lineages', 'neutral', 'neutral_preadapted'
 run_options.trajectory      = 'stochastic'; % 'stochastic' or 'deterministic'
 run_options.annual_cycle    = 'static';     % 'static' or 'seasonal'
 
@@ -23,10 +23,10 @@ run_options.nyear           = 100;
 run_options.nday        	= 365;          
 
 % N.B. not applicable for neutral model
-run_options.npopn           = 77;           % number of phenotypes
+run_options.nphen           = 77;           % number of phenotypes
 run_options.w               = 6;           	% Niche breadth
 run_options.sigma_m         = 0.1;
 
 %% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-run('../EPMD_functions/EPMD_spmd')
+run('EPMD_functions/EPMD_spmd')
