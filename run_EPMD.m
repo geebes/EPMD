f11d18e70d184a171b32b53707fed7be5bf76e3f
Ben Ward <b.a.ward@soton.ac.uk>
2019-11-25 16:17:57 +0000

    ...

--- run_EPMD.m
+++ run_EPMD.m
@@ -1,5 +1,5 @@
 clear
-clc
+% clc
 cd ~/GitHub/EPMD
 addpath(genpath('~/GitHub/EPMD'))
 
@@ -8,16 +8,18 @@ addpath EPMD_functions
 %% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 % set options
-run_options.TM_scheme       = 'GUD_X01_weighted_transport'; % 'surface_transport' or 'GUD_X01_weighted_transport', or similar
-run_options.seed_dist       = 'selective_dispersal';    % 'preadapted', 'equal', 'lineages', 'neutral', 'neutral_preadapted'
+run_options.TM_scheme       = 'GUD_X17_weighted_transport'; % 'surface_transport' or 'GUD_X01_weighted_transport', or similar
+run_options.seed_dist       = 'selective_dispersal';    % 'preadapted', 'equal', 'lineages', 'neutral', 'selective_dispersal'
 run_options.trajectory      = 'stochastic'; % 'stochastic' or 'deterministic'
 run_options.annual_cycle    = 'static';     % 'static' or 'seasonal'
+run_options.seedseed        = 2;            % seed for global seeding sites
 
 % note depth-integrated TMs are each associated with a particular abundance distribution
 % (this will be overwritten if using depth-integrated biomass-weighted transport)
 run_options.DARWIN_pop      = 'X01'; 
 
 run_options.save_data       = true;
+run_options.xsparse         = true;  % run with x as sparse (or full)
 
 run_options.nyear           = 100;          
 run_options.nday        	= 365;          
@@ -25,7 +27,7 @@ run_options.nday        	= 365;
 % N.B. not applicable for neutral model
 run_options.nphen           = 77;           % number of phenotypes
 run_options.w               = 6;           	% Niche breadth
-run_options.sigma_m         = 0.1;
+run_options.sigma_m         = 0.1;          % Mutation size
 
 %% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
